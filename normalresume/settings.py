03fed5a7c48d7aac2ece819344d8763cd8ada1a7
Melissa Yvette Cavazos <109395704+melissacallis@users.noreply.github.com>
2025-07-18 01:27:27 +0000

    created /staticfiles

--- normalresume/settings.py
+++ normalresume/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 # WhiteNoise for static files in production
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
